670ebddf9cb98254d6ed95141ae81b2b89c2d724
Efterklang <gaojiaxing2004@qq.com>
2023-10-17 16:36:55 +0800

    增加用户状态字段

--- FreeChatClient/src/main/java/common/Message.java
+++ FreeChatClient/src/main/java/common/Message.java
@@ -4,13 +4,22 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    /*
+     * Version 1.0
+     * 功能：表示 客户端和服务端 通信时的消息对象
+     * 包括5个字段：发送者、接收者、发送内容、发送时间、消息类型
+    */
     private String sender;
     private String getter; // 接收者 
 	private String content;// 发送内容 
 	private String sendTime;// 发送时间
     private String messType;// 消息类型,在MessageType接口中定义
-
+    /*
+     * Version 2.0
+     * 功能：增加用户状态字段
+     */
+    private String state;
+    
     // getter and setter
     public static long getSerialversionuid() {
         return serialVersionUID;
@@ -45,7 +54,10 @@ public class Message implements Serializable {
     public void setMessType(String messType) {
         this.messType = messType;
     }
-
-    
-
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
 }
