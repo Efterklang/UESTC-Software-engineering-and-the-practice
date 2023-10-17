2c13c704fcafb0c030f8e5c399cefcdb3c13581d
Efterklang <gaojiaxing2004@qq.com>
2023-10-17 22:45:24 +0800

    完成界面的绘制

--- FreeChatClient/src/main/java/client/view/LoginFrame.java
+++ FreeChatClient/src/main/java/client/view/LoginFrame.java
@@ -1,54 +1,61 @@
 package client.view;
-
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+/**
+ * @author GaoJiaxing
+ * @Version 1.0
+ */
+
 /*
- * 客户端登录框架
- * 
- * 
+ * Swing组件介绍
  * BackGroundInfo(JFrame组件):
- * JMenuBar：菜单栏，可以在其中添加菜单和菜单项。
- * JToolBar：工具栏，可以在其中添加按钮和其他组件。
  * JPanel：面板，可以在其中添加其他组件，用于组织和布局其他组件。
  * JLabel：标签，用于显示文本或图像。
  * JButton：按钮，用于触发事件。
  * JTextField：文本框，用于输入和显示文本。
- * JTextArea：文本区域，用于输入和显示多行文本。
- * JScrollPane：滚动面板，用于显示带有滚动条的组件。
- * JComboBox：下拉列表框，用于选择一个选项。
- * JCheckBox：复选框，用于选择一个或多个选项。
- * JRadioButton：单选按钮，用于选择一个选项。
- * JTable：表格，用于显示和编辑表格数据。
- * JFileChooser：文件选择器，用于选择文件或目录
  */
+@SuppressWarnings("all")
 public class LoginFrame extends JFrame {
-    /*
-     * Version 1.0: 
-     *  绘制顶部背景图片＆底部登录栏
-     *  Date: 2023.10.17
-     *  Author: gjx          
-     */
-    /* 顶部背景 */
-    private JLabel background_North;
-    /* 底部登录栏 */
-    private JPanel loginPanel;
-    private JButton loginButton;
-    
-    public LoginFrame() {
-        /* ①顶部背景图片 */
+        /* ① 顶部背景 */
+        private JLabel background_North; // 顶部背景图片
+        /* ② 登录栏 */
+        private JPanel loginPanel; // 存放登录栏组件
+        private JButton loginButton; // 存放登录按钮的图片
+        /* ③ Center */
+        private JTabbedPane choosePane;
+        private JPanel centerPanel;
+
+        private JLabel avatar;
+        // 中部输入Panel
+        private JPanel inputPanel;
+        private JLabel userLabel, pwdLabel, emptyLabel;
+        private JTextField userText; // 用户名输入
+        private JPasswordField pwdTxt; // 密码输入
+        private JButton clearButton, registerButton;
+
+        public LoginFrame() {
+                /* ① 顶部背景图片 */
         ImageIcon background = new ImageIcon();
         background.setImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/background.png"))
                 .getScaledInstance(516, 170, Image.SCALE_DEFAULT));
         background_North = new JLabel(background);
-        /* ②登录栏 */
+        /* ② 登录栏 */
         loginPanel = new JPanel(); // 存放登录栏组件
         loginButton = new JButton(); // 存放登录按钮的图片
         /* 登录按钮图片 */
@@ -60,12 +67,51 @@ public class LoginFrame extends JFrame {
         loginButton.setBorder(null);
         loginButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // 鼠标样式修改为手型，意为可以点击
         loginPanel.add(loginButton);
-        /* ③Center */
+        /* ③ Center */
+        // 3.1 Center-Left
+        ImageIcon headIcon = new ImageIcon();
+        headIcon.setImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/headshot.jpg"))
+                        .getScaledInstance(120, 144, Image.SCALE_DEFAULT));
+        avatar = new JLabel(headIcon);
+        emptyLabel = new JLabel("              ");
+        // 3.2.1 输入栏Panel
+        inputPanel = new JPanel();
+        centerPanel = new JPanel();
+        centerPanel.add(avatar);
+        centerPanel.add(emptyLabel);
+        centerPanel.add(inputPanel);
+        inputPanel.setSize(200, 300);
+        choosePane = new JTabbedPane();
+        choosePane.add("用户登录", centerPanel);
         
-
+        // 3.2.2 输入栏组件
+        userLabel = new JLabel("用户名", JLabel.CENTER);
+        userLabel.setFont(new Font("youyuan", Font.BOLD, 16));
+        pwdLabel = new JLabel("密  码", JLabel.CENTER);
+        pwdLabel.setFont(new Font("youyuan", Font.BOLD, 16));
+        userText = new JTextField();
+        userText.setFont(new Font("youyuan", Font.PLAIN, 14));
+        pwdTxt = new JPasswordField();
+        // 3.2.3 输入栏组件清空 和 注册账号按钮
+        clearButton = new JButton("清除号码");
+        clearButton.setFont(new Font("youyuan", Font.PLAIN, 13));
+        clearButton.setForeground(Color.RED);
+        registerButton = new JButton("注册账号");
+        registerButton.setFont(new Font("youyuan", Font.PLAIN, 13));
+        registerButton.setForeground(Color.BLUE);
+        registerButton.setPreferredSize(new Dimension(20, 20));
+        // 3.2.4 输入栏布局 3*3网格
+        inputPanel.setLayout(new GridLayout(3, 3));
+        inputPanel.add(userLabel);
+        inputPanel.add(userText);
+        inputPanel.add(pwdLabel);
+        inputPanel.add(pwdTxt);
+        inputPanel.add(clearButton);
+        inputPanel.add(registerButton);
         /* 设计界面布局 */
         add(background_North, BorderLayout.NORTH); // 顶部背景
         add(loginPanel, BorderLayout.SOUTH); // 底部登录栏
+        add(choosePane, BorderLayout.CENTER); // 中部登录栏
         setVisible(true);
         setBounds(500, 150, 500, 460);
         setResizable(false);
