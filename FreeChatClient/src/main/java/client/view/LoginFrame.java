17e1139734cf45160dfe55e8373dffc6a34dba74
Efterklang <gaojiaxing2004@qq.com>
2023-10-19 14:32:04 +0800

    implements actionPerformed method

--- FreeChatClient/src/main/java/client/view/LoginFrame.java
+++ FreeChatClient/src/main/java/client/view/LoginFrame.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -17,6 +19,7 @@ import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 /**
  * @author GaoJiaxing
@@ -32,7 +35,7 @@ import javax.swing.JLabel;
  * JTextField：文本框，用于输入和显示文本。
  */
 @SuppressWarnings("all")
-public class LoginFrame extends JFrame {
+public class LoginFrame extends JFrame implements ActionListener {
         /* ① 顶部背景 */
         private JLabel background_North; // 顶部背景图片
         /* ② 登录栏 */
@@ -48,7 +51,8 @@ public class LoginFrame extends JFrame {
         private JTextField userText; // 用户名输入
         private JPasswordField pwdTxt; // 密码输入
         private JButton clearButton, registerButton; // 清空、注册
-
+        // 业务逻辑(Login & Register)
+        private clientService clientService = null; // 用户登录注册类
         public LoginFrame() {
                 /* ① 顶部背景图片 */
                 ImageIcon background = new ImageIcon();
@@ -95,7 +99,7 @@ public class LoginFrame extends JFrame {
                 userText.setFont(new Font("youyuan", Font.PLAIN, 14));
                 pwdTxt = new JPasswordField();
                 // 3.2.3 输入栏组件清空 和 注册账号按钮
-                clearButton = new JButton("清除号码");
+                clearButton = new JButton("清除密码");
                 clearButton.setFont(new Font("youyuan", Font.PLAIN, 13));
                 clearButton.setForeground(Color.RED);
                 registerButton = new JButton("注册账号");
@@ -110,6 +114,10 @@ public class LoginFrame extends JFrame {
                 inputPanel.add(pwdTxt);
                 inputPanel.add(clearButton);
                 inputPanel.add(registerButton);
+                /* 添加监听 */
+                loginButton.addActionListener(this);
+                clearButton.addActionListener(this);
+                registerButton.addActionListener(this);
                 /* 设计界面布局 */
                 add(background_North, BorderLayout.NORTH); // 顶部背景
                 add(loginPanel, BorderLayout.SOUTH); // 底部登录栏
@@ -120,4 +128,35 @@ public class LoginFrame extends JFrame {
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 setTitle("FreeChat_登录");
         }
+        
+        /*
+         * 监听action，添加对应的actionPerformance
+         * 事件源:loginButton,registerButton,clearButton
+         */ 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+                /*
+                 * 如果账号/密码为空，则弹出提示框
+                 */
+                if (e.getSource() == loginButton) {
+                        String userId = userText.getText().trim();
+                        String pwd = new String(pwdTxt.getPassword()).trim();
+                        if ("".equals(userId) || userId == null) {
+                                JOptionPane.showMessageDialog(null, "请输入账号");
+                                return;
+                        }
+                        if ("".equals(pwd) || pwd == null) {
+                                JOptionPane.showMessageDialog(null, "请输入密码");
+                                return;
+                        }
+                        clientService = new clientService();
+                        //TODO 
+                } else if (e.getSource() == registerButton) {
+                        this.setVisible(false);
+                        //TODO new RegisterFrame();
+                } else if (e.getSource() == clearButton) {
+                        pwdTxt.setText("");
+                } 
+        }
+
 }
