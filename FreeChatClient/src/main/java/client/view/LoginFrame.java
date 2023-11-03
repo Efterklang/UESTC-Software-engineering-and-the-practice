864deed1b69f502cfc794c810eebb3e9db7d7846
Efterklang <gaojiaxing2004@qq.com>
2023-11-04 00:43:10 +0800

    🎉登录界面层，version1.0

--- FreeChatClient/src/main/java/client/view/LoginFrame.java
+++ FreeChatClient/src/main/java/client/view/LoginFrame.java
@@ -29,17 +29,13 @@ import javax.swing.JOptionPane;
  * @version 1.0
  * @see client.server.UserClientService
  */
-public class LoginFrame extends JFrame{
-	/* ① 顶部背景 */
+public class LoginFrame extends JFrame {
 	private JLabel background_North; // 顶部背景图片
-	/* ② 登录栏 */
 	private JPanel loginPanel; // 存放登录栏组件
 	private JButton loginButton; // 存放登录按钮的图片
-	/* ③ Center */
-	private JPanel centerPanel; // 中部Panel
+	private JPanel centerPanel = new JPanel(); // 中部Panel
 	private JLabel avatar; // 中部logo
-	// 中部输入Panel
-	private JPanel inputPanel; // 输入栏
+	private JPanel inputPanel = new JPanel(); // 输入栏
 	private JLabel userLabel, pwdLabel; // 用户名、密码
 	private JTextField userText; // 用户名输入
 	private JPasswordField pwdTxt; // 密码输入
@@ -47,21 +43,57 @@ public class LoginFrame extends JFrame{
 	// 业务逻辑(Login & Register)
 	private UserClientService userClientService = null; // 用户登录注册类
 
-	/**
-	 * @main 用于调试代码
-	 */
-	public static void main(String[] args) {
-		new LoginFrame();
-	}
-
 	public LoginFrame() {
-		/* ① 顶部背景图片 */
+		/* 顶部背景图片 */
 		ImageIcon background = new ImageIcon();
 		background.setImage(
 				Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/background.png"))
 						.getScaledInstance(516, 170, Image.SCALE_DEFAULT));
 		background_North = new JLabel(background);
-		/* ② 登录栏 */
+		
+		/* CenterPanel: avatar,空Panel(界面留白),inputPanel */
+		// avatar
+		ImageIcon avatarIcon = new ImageIcon();
+		avatarIcon.setImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/headshot.jpg"))
+				.getScaledInstance(140, 140, Image.SCALE_DEFAULT));
+		avatar = new JLabel(avatarIcon);
+		// inputPanel
+		inputPanel.setSize(400, 300);
+		inputPanel.setLayout(new GridLayout(5, 2));
+
+		userLabel = new JLabel("用户名", JLabel.CENTER);
+		userLabel.setFont(new Font("youyuan", Font.BOLD, 20));
+		pwdLabel = new JLabel("密  码", JLabel.CENTER);
+		pwdLabel.setFont(new Font("youyuan", Font.BOLD, 20));
+
+		userText = new JTextField();
+		userText.setFont(new Font("youyuan", Font.PLAIN, 20));
+		pwdTxt = new JPasswordField();
+
+		clearButton = new JButton("清除密码");
+		clearButton.setFont(new Font("youyuan", Font.PLAIN, 15));
+		clearButton.setForeground(Color.RED);
+		registerButton = new JButton("注册账号");
+		registerButton.setFont(new Font("youyuan", Font.PLAIN, 15));
+		registerButton.setForeground(Color.BLUE);
+		registerButton.setPreferredSize(new Dimension(20, 20));
+
+		inputPanel.add(userLabel);
+		inputPanel.add(userText);
+		inputPanel.add(new JLabel());
+		inputPanel.add(new JLabel());
+		inputPanel.add(pwdLabel);
+		inputPanel.add(pwdTxt);
+		inputPanel.add(new JLabel());
+		inputPanel.add(new JLabel());
+		inputPanel.add(clearButton);
+		inputPanel.add(registerButton);
+		
+		centerPanel.add(avatar);
+		centerPanel.add(new JLabel("            "));
+		centerPanel.add(inputPanel);
+
+		/* 登录栏 */
 		loginPanel = new JPanel();
 		loginButton = new JButton();
 		/* 登录按钮 */
@@ -73,46 +105,18 @@ public class LoginFrame extends JFrame{
 		loginButton.setBorderPainted(false);
 		loginButton.setBorder(null);
 		loginPanel.add(loginButton);
-		/* ③ Center */
-		// 3.1 Center-Left
-		ImageIcon avatarIcon = new ImageIcon();
-		avatarIcon.setImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/headshot.jpg"))
-				.getScaledInstance(120, 144, Image.SCALE_DEFAULT));
-		avatar = new JLabel(avatarIcon);
-		// 3.2.1 输入栏Panel
-		inputPanel = new JPanel();
-		centerPanel = new JPanel();
-		centerPanel = new JPanel();
-		centerPanel.add(avatar);
-		centerPanel.add(new JLabel("            "));
-		centerPanel.add(inputPanel);
-		inputPanel.setSize(200, 300);
-		// 3.2.2 输入栏组件
-		userLabel = new JLabel("用户名", JLabel.CENTER);
-		userLabel.setFont(new Font("youyuan", Font.BOLD, 20));
-		pwdLabel = new JLabel("密  码", JLabel.CENTER);
-		pwdLabel.setFont(new Font("youyuan", Font.BOLD, 20));
-		userText = new JTextField();
-		userText.setFont(new Font("youyuan", Font.PLAIN, 20));
-		pwdTxt = new JPasswordField();
-		// 3.2.3 输入栏组件清空 和 注册账号按钮
-		clearButton = new JButton("清除密码");
-		clearButton.setFont(new Font("youyuan", Font.PLAIN, 15));
-		clearButton.setForeground(Color.RED);
-		registerButton = new JButton("注册账号");
-		registerButton.setFont(new Font("youyuan", Font.PLAIN, 15));
-		registerButton.setForeground(Color.BLUE);
-		registerButton.setPreferredSize(new Dimension(20, 20));
-		// 3.2.4 输入栏布局 3*3网格
-		inputPanel.setLayout(new GridLayout(4, 2));
-		inputPanel.add(new JLabel());
-		inputPanel.add(new JLabel());
-		inputPanel.add(userLabel);
-		inputPanel.add(userText);
-		inputPanel.add(pwdLabel);
-		inputPanel.add(pwdTxt);
-		inputPanel.add(clearButton);
-		inputPanel.add(registerButton);
+
+		/* 设计界面布局 */
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		add(background_North, BorderLayout.NORTH);
+		add(centerPanel, BorderLayout.CENTER);
+		add(loginPanel, BorderLayout.SOUTH);
+		setVisible(true);
+		setBounds(600, (int)(screenSize.getHeight()/2) - 315, 500, 430);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("FreeChat_登录");
+
 		/* 添加监听 */
 		loginButton.addActionListener(new ActionListener() {
 			@Override
@@ -151,14 +155,5 @@ public class LoginFrame extends JFrame{
 				pwdTxt.setText("");
 			}
 		});
-		/* 设计界面布局 */
-		add(background_North, BorderLayout.NORTH); // 顶部背景
-		add(loginPanel, BorderLayout.SOUTH); // 底部登录栏
-		add(centerPanel, BorderLayout.CENTER);
-		setVisible(true);
-		setBounds(500, 150, 500, 460);
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("FreeChat_登录");
 	}
 }
