3f8ea1dcb6e07fef37fea919993b6048fbc9bb77
Efterklang <gaojiaxing2004@qq.com>
2023-11-04 15:41:54 +0800

    MessageType，定义类型常量

--- FreeChatServer/src/main/java/common/MessageType.java
+++ FreeChatServer/src/main/java/common/MessageType.java
@@ -2,18 +2,20 @@ package common;
 
 /**
  * @apiNote 消息类型常量类
- * @LOGIN_SUCCEED 登录成功
- * @LOGIN_FAIL 登录失败
- * @COMMON_MES 普通消息包(私聊)
- * @GET_ONLINE_FRIEND 客户端 请求返回在线消息列表
- * @RETURN_ONLINE_FRIEND 服务端 返回在线用户列表
- * @CLIENT_EXIT 客户端请求退出
- * @GROUP_MES 客户端群发消息
- * @DIRECT_FILE_MES 客户端 发送文件
- * @REGIST_REQUEST 客户端 请求注册账户
- * @REGIST_SUCCEED 注册成功
- * @REGIST_FAIL 注册失败
- * @GROUP_FILE_MES 客户端 群发文件
+ * <ul>
+ * <li>LOGIN_SUCCEED 登录成功
+ * <li>LOGIN_FAIL 登录失败
+ * <li>COMMON_MES 普通消息包(私聊)
+ * <li>GET_ONLINE_FRIEND 客户端 请求返回在线消息列表
+ * <li>RETURN_ONLINE_FRIEND 服务端 返回在线用户列表
+ * <li>CLIENT_EXIT 客户端请求退出
+ * <li>GROUP_MES 客户端群发消息
+ * <li>DIRECT_FILE_MES 客户端 发送文件
+ * <li>REGIST_REQUEST 客户端 请求注册账户
+ * <li>REGIST_SUCCEED 注册成功
+ * <li>REGIST_FAIL 注册失败
+ * <li>GROUP_FILE_MES 客户端 群发文件
+ * </ul>
  */
 public interface MessageType {
     String LOGIN_SUCCEED = "1"; // 登录成功
