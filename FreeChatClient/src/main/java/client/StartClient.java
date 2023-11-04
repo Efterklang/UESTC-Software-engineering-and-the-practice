4a632d6f99f9216057913f2278bada4b2404805d
Efterklang <gaojiaxing2004@qq.com>
2023-11-04 14:43:07 +0800

    🎉启动客户端程序

--- FreeChatClient/src/main/java/client/StartClient.java
+++ FreeChatClient/src/main/java/client/StartClient.java
@@ -2,6 +2,11 @@ package client;
 
 import client.view.LoginFrame;
 
+/**
+ * @author gjx
+ * @apiNote 客户端启动类，启动登录窗口
+ * @version 1.0
+ */
 public class StartClient {
     public static void main(String[] args) {
         new LoginFrame();
