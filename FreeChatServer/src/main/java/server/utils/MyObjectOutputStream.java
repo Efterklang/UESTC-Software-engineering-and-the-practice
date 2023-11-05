1b9647551bc9f5e8c600267f1543a56277c03abc
Efterklang <gaojiaxing2004@qq.com>
2023-11-05 20:31:05 +0800

    util

--- FreeChatServer/src/main/java/server/utils/MyObjectOutputStream.java
+++ FreeChatServer/src/main/java/server/utils/MyObjectOutputStream.java
@@ -4,21 +4,18 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
-public class MyObjctOutputStream {
+public class MyObjectOutputStream extends ObjectOutputStream {
     /*
      * How to Fix java.io.StreamCorruptedException: invalid type code in Java?
      * https://www.geeksforgeeks.org/how-to-fix-java-io-streamcorruptedexception-
      * invalid-type-code-in-java/
      */
-    public class MyObjectOutputStream extends ObjectOutputStream {
-        public MyObjectOutputStream(OutputStream out) throws Exception {
-            super(out);
-        }
-
-        @Override
-        protected void writeStreamHeader() throws IOException {
-            return;
-        }
+    public MyObjectOutputStream(OutputStream out) throws IOException {
+        super(out);
     }
 
+    @Override
+    protected void writeStreamHeader() throws IOException {
+        return;
+    }
 }
