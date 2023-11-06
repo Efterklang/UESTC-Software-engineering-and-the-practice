371410e412fa45a61e808c178bff7f0974f54e08
Efterklang <gaojiaxing2004@qq.com>
2023-11-06 13:08:30 +0800

    🎉Server Database(Hash+I/O)

--- FreeChatServer/src/main/java/server/utils/ProcessAccountData.java
+++ FreeChatServer/src/main/java/server/utils/ProcessAccountData.java
@@ -6,12 +6,32 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.ConcurrentHashMap;
 import common.User;
 
+/**
+ * @apiNote 使用IO流+ConcurrentHashMap模拟数据库，后期可用JDBC+MySQL代替;
+ * <ul>
+ * <li> readAccountFile: 读取数据库(account.properties)中的账号数据，写入到validUsers中;
+ *      当程序启动时，需要调用该方法(放在static {}中)，将数据库中的数据读取到validUsers中;
+ *      当对数据库进行修改时，需要调用该方法，将修改后的数据写入到validUsers中;
+ * <li> writeAccountFile: 当进行注册时，将userId＆pwd写入到数据库中(account.properties)
+ * </ul>
+ * @author gjx
+ * @see common.User
+ * @throws IOException
+ */
 public class ProcessAccountData {
-    String path = "E:/UESTC/Chat/freechat/FreeChatServer/src/main/resources/account.txt"; // "account.txt"
+    ClassLoader classLoader = ProcessAccountData.class.getClassLoader();
+    private URL url = classLoader.getResource("account.properties");
+    private final String path = url.getPath();
     
+    /**
+     * @apiNote 对数据库(account.properties,见target/resource)进行读取，写入到validUsers
+     * @param validUsers 要写入账号数据的HashMap
+     * @throws IOException
+     */
     public void readAccountFile(ConcurrentHashMap<String, User> validUsers) {
         File file = new File(path);
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -28,25 +48,30 @@ public class ProcessAccountData {
         }
     }
 
+
+    /**
+     * @apiNote 当进行注册时，将userId＆pwd写入到数据库中(account.properties)
+     * @param userId 用户id
+     * @param pwd 用户密码
+     * @throws IOException
+     */
     public void writeAccountFile(String userId, String pwd) {
         File file = new File(path);
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file,true))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
             bw.write(userId + "," + pwd + "\n");
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    // public static void main(String[] args) {
-    //     ConcurrentHashMap<String, User> validUsers = new ConcurrentHashMap<>();
-    //     User user1 = new User("admin", "123456");
-    //     User user2 = new User("admin2", "123");
-    //     ProcessAccountData processAccountData = new ProcessAccountData();
-    //     processAccountData.writeAccountFile("admin", "123456");
-    //     processAccountData.writeAccountFile("admin2", "123");
-
-    //     processAccountData.readAccountFile(validUsers);
-    //     System.out.println(validUsers.get("admin").getPassword());
-    //     System.out.println(validUsers.get("admin2").getPassword());
-    // }
+    //DEBUG
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, User> validUsers = new ConcurrentHashMap<>();
+        ProcessAccountData processAccountData = new ProcessAccountData();
+        processAccountData.writeAccountFile("admin", "123456");
+        processAccountData.writeAccountFile("admin2", "123");
+        processAccountData.readAccountFile(validUsers);
+        System.out.println(validUsers.get("admin").getPassword());
+        System.out.println(validUsers.get("admin2").getPassword());
+    }
 }
